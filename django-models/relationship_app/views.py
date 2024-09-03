c3a560e597789b3f16e87ffd27c374a7af4fa98f
L7dri <ayoubeh98@gmail.com>
2024-09-03 19:58:00 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -45,4 +45,4 @@ def user_register(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
