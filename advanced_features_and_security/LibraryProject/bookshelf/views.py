551c5428fbd42918e6e244e8c40e6f2ea00bf1a8
L7dri <ayoubeh98@gmail.com>
2024-09-04 15:59:14 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# relationship_app/views.py
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
+@permission_required('relationship_app.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/view_books.html', {'books': books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def create_book(request):
+    # Implementation for creating a book
+    pass
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    # Implementation for editing a book
+    pass
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    # Implementation for deleting a book
+    pass
+
 
 # Create your views here.
