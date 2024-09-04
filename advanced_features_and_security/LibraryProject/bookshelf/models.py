f520560be58df5e99a928d0180c7016be2ef6a4b
L7dri <ayoubeh98@gmail.com>
2024-09-04 15:48:13 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,3 +4,16 @@ from django.db import models
         title = models.CharField(max_length=200)
         author = models.CharField(max_length=100)
         published_date = models.IntegerField()
+
+# relationship_app/models.py
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
