3e3d1aa7116de4bfbf598fd2763590d00c9ec272
L7dri <ayoubeh98@gmail.com>
2024-09-04 15:34:35 +0100

    Update settings.py

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -1,7 +1,3 @@
-# Check if the user has the 'Member' role
-def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
+# advanced_features_and_security/settings.py
 
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
+AUTH_USER_MODEL = 'relationship_app.CustomUser'
