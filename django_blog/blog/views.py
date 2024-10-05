e8c9af7affd4a0fc08fd74b9aef1fdaa4ad8f2ff
L7dri <ayoubeh98@gmail.com>
2024-10-05 13:32:58 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -82,3 +82,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+from django.views.generic import DetailView
+from .models import Post
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
