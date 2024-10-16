d88efe16e9d4cf4bc025c9f43452fab1fa320e58
L7dri <ayoubeh98@gmail.com>
2024-10-16 21:29:28 +0200

    Create serializers.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
