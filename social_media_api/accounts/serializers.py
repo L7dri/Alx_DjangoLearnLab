630f7d9dd42c1d826dc115827f2fcebad8b488a0
L7dri <ayoubeh98@gmail.com>
2024-10-16 21:32:18 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,24 @@
+# accounts/serializers.py
 from rest_framework import serializers
-from .models import CustomUser
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+# Get the user model dynamically
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        # Remove the password from the validated data
+        password = validated_data.pop('password')
+        # Create the user without the password first
+        user = User(**validated_data)
+        # Set the password using set_password to ensure it's hashed
+        user.set_password(password)
+        user.save()
         return user
