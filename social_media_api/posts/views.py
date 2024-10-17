428aec8a79cfb5c047c264148d4f1b80281d397f
L7dri <ayoubeh98@gmail.com>
2024-10-17 19:28:48 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,12 +1,14 @@
 # posts/views.py
 from rest_framework import viewsets, permissions
-from .models import Post, Comment
+from .models import Post, Comment, filter
 from .serializers import PostSerializer, CommentSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
